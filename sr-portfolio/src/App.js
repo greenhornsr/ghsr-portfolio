0a4731ea8949a14c3ea15227caeb03a1bb7604e3
greenhornsr <srollins11279@gmail.com>
2019-12-31 11:36:00 -0800

    added portfolio owner content components, adjusted screen boundaries to use view height and width

--- sr-portfolio/src/App.js
+++ sr-portfolio/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import Nav from './components/navcomponents/Nav';
 
-import Logo from './components/navcomponents/Logo';
+// Components
+import Nav from './components/navcomponents/Nav';
+import Name from './components/portfolio-owner/Name';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Name/>
     </div>
   );
 }
