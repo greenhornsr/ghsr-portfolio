7d1345e7bdf591be6d9816550092c37ec2ae0b96
greenhornsr <srollins11279@gmail.com>
2020-03-06 12:40:42 -0800

    added targets to socialmedia component a-tags.  modified color scheme

--- sr-portfolio/src/components/contact/SocialMedia.jsx
+++ sr-portfolio/src/components/contact/SocialMedia.jsx
@@ -3,16 +3,20 @@ import React from 'react'
 const SocialMedia = () => {
     function getResume(){
         window.open(require('/Users/Rollins/Desktop/Steve/Portfolio/ghsr-portfolio/sr-portfolio/src/assets/resume/Steve_Rollins_Resume_2020.pdf')) 
+        const activeElement = document.getElementById('.download-resume')
+        console.log(activeElement)
     }
+
+
     return (
         <div className="social-icons" >
-            <a href="https://github.com/greenhornsr/">
+            <a href="https://github.com/greenhornsr/" target="_blank">
                 <span class="fa-stack fa-3x">
                     <i class="fa fa-circle-thin fa-stack-2x"></i>
                     <i class="fa fa-github fa-stack-1x"></i>
                 </span>
             </a>
-            <a href="https://linkedin.com/in/steven-rollins">
+            <a href="https://linkedin.com/in/steven-rollins" target="_blank">
                 <span class="fa-stack fa-3x">
                     <i class="fa fa-circle-thin fa-stack-2x"></i>
                     <i class="fa fa-linkedin fa-stack-1x"></i>
@@ -27,7 +31,8 @@ const SocialMedia = () => {
             <div className="download-resume" onClick={getResume} >
                 <span class="fa-stack fa-3x">
                     <i class="fa fa-circle-thin fa-stack-2x"></i>
-                    <i class="fa fa-file fa-stack-1x"></i>
+                    {/* <i class="fa fa-file fa-stack-1x"></i> */}
+                    <i class="fa fa-download fa-stack-1x"></i>
                 </span>
             </div>
         </div>
