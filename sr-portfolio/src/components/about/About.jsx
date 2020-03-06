d196adf57c0b585c40108dd647423d4532ec28c9
greenhornsr <srollins11279@gmail.com>
2020-03-06 15:15:13 -0800

    added details to aboutme-data file, added to about component

--- sr-portfolio/src/components/about/About.jsx
+++ sr-portfolio/src/components/about/About.jsx
@@ -22,24 +22,38 @@ const About = ({ aboutme }) => {
                         <section className="lvl2-inner-about">
                             <h3>{se.h3}</h3>
                             <p className="intro"> {se.desc} </p>
+                            <p className="intro"> {se.desc1} </p>
+                            <p className="intro"> {se.desc2} </p>
+                            <p className="intro"> {se.desc3}, <br/> {se.desc4} <br/> {se.desc5} <br/> {se.desc6} </p>
                         </section>
                     </section>
                     <section className="inner-about-sections family">
                         <section className="lvl2-inner-about">
                             <h3> {fi.h3} </h3>
                             <p className="intro"> {fi.desc} </p>
+                            <p className="intro"> {fi.desc1} </p>
+                            <p className="intro"> {fi.desc2} </p>
+                            <p className="intro"> {fi.desc3} </p>
+                            <p className="intro"> {fi.desc4} </p>
                         </section>
                     </section>
                     <section className="inner-about-sections education">
                         <section className="lvl2-inner-about">
                             <h3> {ed.h3} </h3>
-                            <p className="intro"> {ed.desc} </p>    
+                            <p className="intro"> {ed.desc} </p>   
+                            <p className="intro"> {ed.desc1} </p>   
+                            <p className="intro"> {ed.desc2} </p>   
+                            <p className="intro"> {ed.desc3} </p>   
+                            <p className="intro"> {ed.desc4} </p>   
                         </section>
                     </section>
                     <section className="inner-about-sections background">
                         <section className="lvl2-inner-about">
                             <h3> {bg.h3} </h3>
                             <p className="intro"> {bg.desc} </p>
+                            <p className="intro"> {bg.desc1} </p>
+                            <p className="intro"> {bg.desc2} </p>
+                            <p className="intro"> {bg.desc3} </p>
                         </section>
                     </section>
                 </div>
