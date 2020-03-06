f5b9c679018a88e61692b9ce6a25bf475b5338a6
greenhornsr <srollins11279@gmail.com>
2020-03-06 09:29:54 -0800

    removed twitter icon from socialmedia component

--- sr-portfolio/src/components/contact/SocialMedia.jsx
+++ sr-portfolio/src/components/contact/SocialMedia.jsx
@@ -3,24 +3,24 @@ import React from 'react'
 const SocialMedia = () => {
     return (
         <div className="social-icons" >
-            <a href="">
+            <a href="https://github.com/greenhornsr/">
                 <span class="fa-stack fa-3x">
                     <i class="fa fa-circle-thin fa-stack-2x"></i>
-                    <i class="fa fa-twitter fa-stack-1x"></i>
+                    <i class="fa fa-github fa-stack-1x"></i>
                 </span>
             </a>
-            <a href="">
-                <span class="fa-stack fa-3x">
-                    <i class="fa fa-circle-thin fa-stack-2x"></i>
-                    <i class="fa fa-facebook fa-stack-1x"></i>
-                </span>
-            </a>
-            <a href="">
+            <a href="https://linkedin.com/in/steven-rollins">
                 <span class="fa-stack fa-3x">
                     <i class="fa fa-circle-thin fa-stack-2x"></i>
                     <i class="fa fa-linkedin fa-stack-1x"></i>
                 </span>
             </a>
+            {/* <a href="">
+                <span class="fa-stack fa-3x">
+                    <i class="fa fa-circle-thin fa-stack-2x"></i>
+                    <i class="fa fa-twitter fa-stack-1x"></i>
+                </span>
+            </a> */}
         </div>
     )
 }
