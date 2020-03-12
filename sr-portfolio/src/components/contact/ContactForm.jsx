d383ef29a7fccba89fef14949dc3461e6b9f0fd7
greenhornsr <srollins11279@gmail.com>
2020-03-11 20:35:58 -0700

    added tablet and phone responsive styling for contact component

--- sr-portfolio/src/components/contact/ContactForm.jsx
+++ sr-portfolio/src/components/contact/ContactForm.jsx
@@ -13,6 +13,8 @@ const ContactForm = () => {
                 <h2>Contact Me</h2>
                 <SocialMedia />
                 <p>If you would like to learn more about my past projects, visit my github.  Learn more about my professional background, visit my LinkedIn.  I have also added a convenient link to email me directly and download my resume.</p>
+                <p>Thank you,</p>
+                <p>Steven D. Rollins</p>
                 {/* <form action="submit">
                     <label> Name: 
                         <input type="text" name="name" />    
