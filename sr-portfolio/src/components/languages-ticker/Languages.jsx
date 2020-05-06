9e95090bdc5be750ed5c986847a3a71da7d6f85d
greenhornsr <srollins11279@gmail.com>
2020-05-06 10:35:58 -0700

    added hover effect for contact page icons

--- sr-portfolio/src/components/languages-ticker/Languages.jsx
+++ sr-portfolio/src/components/languages-ticker/Languages.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 const Languages = (props) => {
     console.log(props)
     return (
-        <div className="MARQUEE">  
+        <>
+        {/* <div className="MARQUEE">   */}
             {/* <section className="marq-div1">
                 <div className="marq-div-img"><img src={props.icon.icon} alt=""/></div> */}
             <p>{props.icon.lang}</p>
             {/* </section> */}
-        </div>
+        {/* </div> */}
+        </>
     )
 }
 
