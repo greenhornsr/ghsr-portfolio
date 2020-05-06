44d35ed3c31df9ce5dc32edbf9dec233b6bde228
greenhornsr <srollins11279@gmail.com>
2020-05-06 10:52:47 -0700

    changed accent color from green to red, added spacing to contact me page, added hover effect red and pointer to social icons on contact page.

--- sr-portfolio/src/components/contact/SocialMedia.jsx
+++ sr-portfolio/src/components/contact/SocialMedia.jsx
@@ -9,31 +9,37 @@ const SocialMedia = () => {
 
     return (
         <div className="social-icons" >
-            <a href="https://github.com/greenhornsr/" target="_blank">
-                <span class="fa-stack fa-3x">
-                    <i class="fa fa-circle-thin fa-stack-2x"></i>
-                    <i class="fa fa-github fa-stack-1x"></i>
-                </span>
-            </a>
-            <a href="https://linkedin.com/in/steven-rollins" target="_blank">
-                <span class="fa-stack fa-3x">
-                    <i class="fa fa-circle-thin fa-stack-2x"></i>
-                    <i class="fa fa-linkedin fa-stack-1x"></i>
-                </span>
-            </a>
-            <a href="mailto:coding61.sr@gmail.com?Subject=Job%20Opportunity&body=I%20am%20interested%20in%20connecting!" target="_top">
-                <span class="fa-stack fa-3x">
-                    <i class="fa fa-circle-thin fa-stack-2x"></i>
-                    <i class="fa fa-envelope fa-stack-1x"></i>
-                </span>
-            </a>
-            <div className="download-resume" onClick={getResume} >
+            <section>
+                <a href="https://github.com/greenhornsr/" target="_blank">
+                    <span class="fa-stack fa-3x">
+                        <i class="fa fa-circle-thin fa-stack-2x"></i>
+                        <i class="fa fa-github fa-stack-1x"></i>
+                    </span>
+                </a>
+            </section>
+            <section>
+                <a href="https://linkedin.com/in/steven-rollins" target="_blank">
+                    <span class="fa-stack fa-3x">
+                        <i class="fa fa-circle-thin fa-stack-2x"></i>
+                        <i class="fa fa-linkedin fa-stack-1x"></i>
+                    </span>
+                </a>
+            </section>
+            <section>
+                <a href="mailto:coding61.sr@gmail.com?Subject=Job%20Opportunity&body=I%20am%20interested%20in%20connecting!" target="_top">
+                    <span class="fa-stack fa-3x">
+                        <i class="fa fa-circle-thin fa-stack-2x"></i>
+                        <i class="fa fa-envelope fa-stack-1x"></i>
+                    </span>
+                </a>
+            </section>
+            <section className="download-resume" onClick={getResume} >
                 <span class="fa-stack fa-3x">
                     <i class="fa fa-circle-thin fa-stack-2x"></i>
                     {/* <i class="fa fa-file fa-stack-1x"></i> */}
                     <i class="fa fa-download fa-stack-1x"></i>
                 </span>
-            </div>
+            </section>
         </div>
     )
 }
