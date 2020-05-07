0c2792b73936283c16fff3c1a1168f66b2dfa604
greenhornsr <srollins11279@gmail.com>
2020-05-06 19:28:11 -0700

    adjusted sytlings in portfolio owner and contact components

--- sr-portfolio/src/components/contact/SocialMedia.jsx
+++ sr-portfolio/src/components/contact/SocialMedia.jsx
@@ -9,7 +9,7 @@ const SocialMedia = () => {
 
     return (
         <div className="social-icons" >
-            <section>
+            <section className="sm-sections" >
                 <a href="https://github.com/greenhornsr/" target="_blank">
                     <span className="fa-stack fa-3x">
                         <i className="fa fa-circle-thin fa-stack-2x"></i>
@@ -17,7 +17,7 @@ const SocialMedia = () => {
                     </span>
                 </a>
             </section>
-            <section>
+            <section className="sm-sections" >
                 <a href="https://linkedin.com/in/steven-rollins" target="_blank">
                     <span className="fa-stack fa-3x">
                         <i className="fa fa-circle-thin fa-stack-2x"></i>
@@ -25,7 +25,7 @@ const SocialMedia = () => {
                     </span>
                 </a>
             </section>
-            <section>
+            <section className="sm-sections" >
                 <a href="mailto:coding61.sr@gmail.com?Subject=Job%20Opportunity&body=I%20am%20interested%20in%20connecting!" target="_top">
                     <span className="fa-stack fa-3x">
                         <i className="fa fa-circle-thin fa-stack-2x"></i>
@@ -33,7 +33,7 @@ const SocialMedia = () => {
                     </span>
                 </a>
             </section>
-            <section className="download-resume" onClick={getResume} >
+            <section className="download-resume sm-sections" onClick={getResume} >
                 <span className="fa-stack fa-3x">
                     <i className="fa fa-circle-thin fa-stack-2x"></i>
                     {/* <i class="fa fa-file fa-stack-1x"></i> */}
