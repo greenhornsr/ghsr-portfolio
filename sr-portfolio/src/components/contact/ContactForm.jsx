39dd8b6003c1dd45b9b1a0c8c93bcdc4e3a0c9e4
greenhornsr <srollins11279@gmail.com>
2020-05-07 10:10:50 -0700

    removed form input

--- sr-portfolio/src/components/contact/ContactForm.jsx
+++ sr-portfolio/src/components/contact/ContactForm.jsx
@@ -15,21 +15,6 @@ const ContactForm = () => {
                 <p>If you would like to learn more about my past projects, visit my github.  Learn more about my professional background, visit my LinkedIn.  I have also added a convenient link to email me directly and <a href="https://steven-d-rollins-portfolio.netlify.app/static/media/Steve_Rollins_Resume_MAY_2020.836e730c.pdf" target="_blank">download my resume.</a></p>
                 <p>Thank you,</p>
                 <p>Steven D. Rollins</p>
-                {/* <form action="submit">
-                    <label> Name: 
-                        <input type="text" name="name" />    
-                    </label>
-                    <label> Email: 
-                        <input type="email" name="email" />    
-                    </label>
-                    <label> Phone: 
-                        <input type="tel" name="phone" />    
-                    </label>
-                    <label> file: 
-                        <input type="file" name="file" />    
-                    </label>
-                    <input type="image" src="../navcomponents/Logo" alt="submit" />    
-                </form> */}
             </div>
         </div>
     )
