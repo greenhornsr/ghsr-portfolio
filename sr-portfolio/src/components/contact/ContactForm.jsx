35b8f90059082a1b10260d849cc92831ef4703f6
greenhornsr <srollins11279@gmail.com>
2022-07-03 14:11:18 -0700

    updated link to properly reflect deployment url

--- sr-portfolio/src/components/contact/ContactForm.jsx
+++ sr-portfolio/src/components/contact/ContactForm.jsx
@@ -12,7 +12,7 @@ const ContactForm = () => {
             <div className="contact-body">
                 <h2>Contact Me</h2>
                 <SocialMedia />
-                <p>If you would like to learn more about my past projects, visit my github.  Learn more about my professional background, visit my LinkedIn.  I have also added a convenient link to email me directly and <a href="http://localhost:3000/static/media/Steve_Rollins_Resume_July_2022.19f95230.pdf" target="_blank" rel="noopener noreferrer" >download my resume.</a></p>
+                <p>If you would like to learn more about my past projects, visit my github.  Learn more about my professional background, visit my LinkedIn.  I have also added a convenient link to email me directly and <a href="https://www.steven-rollins.me/static/media/Steve_Rollins_Resume_July_2022.19f95230.pdf" target="_blank" rel="noopener noreferrer" >download my resume.</a></p>
                 <p>Thank you,</p>
                 <p>Steven D. Rollins</p>
             </div>
